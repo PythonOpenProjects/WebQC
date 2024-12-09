7c7212e9f713f8fbe170c23730e822d1d0ff2758
PythonOpenProjects <128476143+PythonOpenProjects@users.noreply.github.com>
2024-12-09 11:33:36 +0100

    Add files via upload

--- webqc.py
+++ webqc.py
@@ -188,8 +188,6 @@ def qc():
             
             if chartFilter:
                 chart = (
-                    #alt.Chart(df)
-                    # Uncomment the following line if use away from streamlit.app
                     alt.Chart(dataframe_explorer(df, case=False))
                     .mark_circle()
                     .encode(
@@ -329,8 +327,6 @@ def loadDataEditor():
                 if title != '':
                     df[title] = ''
                 
-        #st.data_editor(df, num_rows="dynamic")
-        # Uncomment the following line if use away from streamlit.app   
         st.data_editor(dataframe_explorer(df, case=False), num_rows="dynamic")
         #clean_data() 
     else:
